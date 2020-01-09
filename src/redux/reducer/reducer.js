428aed1beb4415d2de8c15ddef1deb0205ac81c1
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2020-01-09 13:58:29 +0400

    Reducer changed into switch statement

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,26 +1,24 @@
 const initialState = {
-    articles: [{ title: "Java", id: 1 }, { title: "Android", id: 1 }],
+	articles: [{ title: "Java", id: 1 }, { title: "Android", id: 1 }],
 }
 
 function rootReducer(state = initialState, action) {
-    // switch (action.type) {
-    //     case "ADD_ARTICLE":
-    //         return Object.assign({}, state, {
-    //             articles: state.articles.concat(action.payload)
-    //         });
-    //     default:
-    //         return null;
-    // }
+	// if (action.type === "ADD_ARTICLE") {
+	// 	// Wrong way
+	// 	// state.articles.push(action.payload);
+	// 	return Object.assign({}, state, {
+	// 		articles: state.articles.concat(action.payload)
+	// 	});
+	// }
 
-    if (action.type === "ADD_ARTICLE") {
-        // Wrong way
-        // state.articles.push(action.payload);
-        return Object.assign({}, state, {
-            articles: state.articles.concat(action.payload)
-        });
-    }
-
-    return state;
+	switch (action.type) {
+		case 'ADD_ARTICLE':
+			return Object.assign({}, state, {
+				articles: state.articles.concat(action.payload)
+			});
+		default:
+			return state;
+	}
 }
 
 export default rootReducer;
